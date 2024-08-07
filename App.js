e4f86c941d17d45d4362bf3cac46715a1ff8970c
Aurélien Girault <me@agirault.com>
2024-08-07 14:13:10 +0200

    :sparkles: Feat: building 1st screen

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import ImageViewer from './components/ImageViewer'
+import Button from './components/Button'
+
+// eslint-disable-next-line no-undef
+const placeholderImage = require('./assets/images/background-image.png')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.container}>
+        <ImageViewer placeholderImageSource={placeholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   )
@@ -13,8 +24,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 })
--- components/Button.js
+++ components/Button.js
@@ -1,7 +1,27 @@
 import PropTypes from 'prop-types'
 import { Pressable, View, Text, StyleSheet  } from 'react-native'
+import FontAwesome from '@expo/vector-icons/FontAwesome'
 
-export default function Button({ label }) {
+export default function Button({ label, theme }) {
+  if (theme === 'primary') {
+    return (
+      <View style={[styles.buttonContainer, { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 }]}>
+        <Pressable
+          style={[styles.button, { backgroundColor: '#fff' }]}
+          onPress={() => alert('You pressed a button.')}
+        >
+          <FontAwesome
+            name="picture-o"
+            size={18}
+            color="#25292e"
+            style={styles.buttonIcon}
+          />
+          <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
+        </Pressable>
+      </View>
+    )
+  }
+
   return (
     <View style={styles.buttonContainer}>
       <Pressable style={styles.button} onPress={ () => alert('You pressed a button') }>
@@ -11,7 +31,7 @@ export default function Button({ label }) {
   )
 }
 
-Button.propTypes = { label: PropTypes.string }
+Button.propTypes = { label: PropTypes.string, theme: PropTypes.string }
 
 const styles = StyleSheet.create({
   buttonContainer: {
